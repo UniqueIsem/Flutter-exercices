660b53de904ad692d09ca8be660e5b94546e4a06
Isaac Santos <isaac.emanuel@live.com>
2025-02-13 00:00:59 -0600

    feature(models/expense && expenses_list) creation of expense data model and list view of expenses titles

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,8 +1,10 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
-  
+
   @override
   State<Expenses> createState() {
     return _ExpensesState();
@@ -10,13 +12,28 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      amount: 175,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 98,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children:[
-          Text('The chart'),
-          Text('Expenses list...'),
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
         ],
       ),
     );
