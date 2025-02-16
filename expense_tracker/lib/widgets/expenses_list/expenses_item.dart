220e2b0ffcc783469584f8e136f468e237d4c848
Isaac Santos <isaac.emanuel@live.com>
2025-02-15 23:15:23 -0600

    feature(expenses_list folder) aranged widget files +expenses_item.dart

---
+++ expense_tracker/lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,15 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(context) {
+    return Card(
+      child: Text(expense.title),
+    );
+  }
+}
--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
+
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -8,6 +10,9 @@ class ExpensesList extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return ListView.builder(itemCount: expenses.length, itemBuilder: (ctx, index) => Text(expenses[index].title),);
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpensesItem(expenses[index]),
+    );
   }
 }
